468c4bf6b0f12d14e8db466449379f55eafe276b
ssrimal201-pixel <ssrimal201@gmail.com>
2026-09-17 16:35:25 +0530

    Update index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { View, Text, StyleSheet, ScrollView, TextInput, Image, TouchableOpacity, SafeAreaView } from 'react-native';
 import { useRouter } from 'expo-router';
 import { Search, Info } from 'lucide-react-native';
-import rapData from '../data/rapVerses.json';
 
 export default function HomeScreen() {
   const router = useRouter();
